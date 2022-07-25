b0eade8b802bd26b084dfc9f31949d2f7ff9ad30
Soviet Kitsune <sovietKitsune@soviet.solutions>
2022-07-24 21:55:25 -0700

    Fix typo in base.lua

--- base.lua
+++ base.lua
@@ -103,14 +103,14 @@
 ---| "'warning'"                # Emitted when something went wrong, but your code will probably continue operating normally.
 ---| "'error'"                  # Emitted when something went wrong and your code may not continue operating normally.
 
----When initializing a client, a table of options can be passed to the constructor. A variety of sanity checks are performed on client initialization and while authenticating. Invalid options will result in an error being thrown while unusual options may log a client warning.
+--- When initializing a client, a table of options can be passed to the constructor. A variety of sanity checks are performed on client initialization and while authenticating. Invalid options will result in an error being thrown while unusual options may log a client warning.
 ---@class ClientOptions
 ---@field public routeDelay boolean Minimum time in milliseconds to wait between HTTP requests per-route
 ---@field public maxRetries integer The maximum number of retries to attempt after an HTTP request fails
 ---@field public shardCount integer The total number of shards the application is using (0 signals to use the recommended count)
 ---@field public firstShard integer The ID of the first shard to run on the client (0 is the minimum)
 ---@field public lastShard integer The ID of the last shard to run on the client (-1 signals to use `shardCount - 1`)
----@field public largeThresold integer Limit to how many members are initially fetched per-guild on start-up
+---@field public largeThreshold integer Limit to how many members are initially fetched per-guild on start-up
 ---@field public cacheAllMembers boolean Whether to cache all members (If false, offline members may not be cached)
 ---@field public autoReconnect boolean Whether to attempt to reconnect after an unexpected gateway disconnection
 ---@field public compress boolean Whether to allow for compressed gateway payloads
